442cc1b79c4200b2fc28045c2549a86dc4d432f3
4400113091 <4400113091@BT144-PROG17>
2022-07-11 19:54:20 -0600

    shopping cart test added with outline

---
+++ TestProject2/ShoppingCartTests.cs
@@ -0,0 +1,65 @@
+﻿using CKK.Logic.Models;
+using Xunit;
+using Xunit.Sdk;
+
+namespace StructuredProject1.Logic.TestsForStudents
+{
+    class ShoppingCartTests
+    {
+        [Fact]
+        public void AddingProductTest
+        {
+            try
+            {
+                //assemble
+
+                //act
+
+                //assert
+            }
+
+            catch
+            {
+                //assert failure
+            }
+
+        }
+
+        [Fact]
+        public void RemovingProductTest
+        {
+            try
+            {
+                //assemble
+
+                //act
+
+                //assert
+            }
+
+            catch
+            {
+                //assert failure
+            }
+
+        }
+
+        [Fact]
+        public void GettingTotal
+        {
+            try
+            {
+                //assemble
+
+                //act
+
+                //assert
+            }
+
+            catch
+            {
+                //assert failure
+            }
+        }
+    }
+}
